4a929e013413a2a0673a01412bdca359fc709cff
Денис Дыбин <denisda0507@gmail.com>
2021-07-08 13:43:02 +0300

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Hello World</h1>
+  </div>
+</template>
+
+<script>
+import Default from '@/layouts/default'
+
+export default {
+  components: { 
+    Default
+  }
+}
+</script>
+
+<style>
+
+</style>
